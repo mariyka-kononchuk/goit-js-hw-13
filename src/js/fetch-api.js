1e6aeb9c4c02bbf7eff7071f3e444d5c68cabf1d
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-07-26 17:06:45 +0300

    показывает количество найденных картинок

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -2,52 +2,61 @@ import cardTpl from '../templates/gallery.hbs';
 import refs from "./refs.js";
 const { form, list, card, more } = refs;
 import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
 
+var lightbox = $('.gallery a').simpleLightbox(options);
+
+
+let total = 0;
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
     //зачищаем список отрисовки
     list.innerHTML = "";
     //сбрасываем параметр страницы
     fetchObject.resetPage();
-    console.log(evt.target.elements.searchQuery.value);
+   // console.log(evt.target.elements.searchQuery.value);
     let query = evt.target.elements.searchQuery.value.trim();
+    //обнуляем значение total
+    fetchObject.resetTotal();
     //записываем полученное значение из инпута в свойство объекта с запросом
     fetchObject.setQuery(query);
     //делаем запрос по значению из инпута и отрисовываем первый ответ
     fetchObject.getFetch();
+    //выводим сообщение о том, сколько нашли изображений
+    setTimeout(() => {
+        fetchObject.message();
+  }, 300);
+
+    
     //открываем кнопку загрузки
     more.classList.remove("is-hidden");
     //зачищаем инпут
     form.reset();
 })
-// function generateGallery(array, place) {
-//     const items = array.map((photo) => {
-//         return `<li><img src=${photo.src.tiny} alt=${photo.src.photographer}></li>`
-//     }).join("");
-//     place.insertAdjacentHTML("beforeend", items);
-//     card.innerHTML = cardTpl(items);
-// }
-let total = 0;
-function generateGallery(photo, hit) {
+
+function generateGallery(photo, totalHits) {
     const gallery = cardTpl(photo);
     
     total+= photo.length;
-    
-    console.log(hit);
+    console.log("total2",total);
+    console.log(totalHits);
     console.log(photo.length);
     
     if (photo.length === 0) {
+        more.classList.add("is-hidden");
         return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
-    } else if (hit === total) {
-        
+    } else if (totalHits === total) {
         list.insertAdjacentHTML("beforeend", gallery);
         more.classList.add("is-hidden");
-        return Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        return setTimeout(() => {
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }, 300);
     }
+    //const message = Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
     
     list.insertAdjacentHTML("beforeend", gallery);
     console.log(total);
-    return total;
+    return  total;
 }
 
 const fetchObject = {
@@ -64,35 +73,92 @@ const fetchObject = {
     per_page: 4, //позже сделать 40
     //метод изменения страницы
     query: "",
+    hit:0,
+    resetTotal() {
+        return total=0;
+    },
     setQuery(value) {
+        console.log("setQuery");
         return this.query = value;
     },
     setPage() {
+        console.log("setPage");
         return this.page += 1;
     },
     resetPage() {
+        console.log("resetPage");
         return this.page = 1;
     },
-    getFetch() {
-        let queryParams = `?key=${this.apiKey}&q=${this.query}&image_type=photo&per_page=${this.per_page}&page=${this.page}`;
+    
+    // getFetch() {
+        
+    //     let queryParams = `?key=${this.apiKey}&q=${this.query}&image_type=photo&per_page=${this.per_page}&page=${this.page}&orientation=horizontal&safesearch=true`;
+    //     //готовая строка запроса
+    //     let url = this.baseUrl + queryParams;
+    //     //собственно запрос, который будем обрабатывать методами then, потому что он возвращает промис
+    //     fetch(url).then((response) => {
+    //         //console.log(response);
+    //         return response.json();
+    //     }).then((hits) => {
+    //         console.log(hits);
+    //         let image = hits.hits;
+    //         let totalHits = hits.totalHits;
+    //         console.log(image);
+    //         console.log(totalHits);
+    //         return { image, totalHits };
+    //     }).then(({ image, totalHits }) => {
+    //         console.log(image);
+    //         console.log(totalHits);
+    //         generateGallery(image, totalHits);
+    //         this.hit = totalHits;
+    //         console.log("кол-во картинок", totalHits)
+    //         console.log("хит",this.hit);
+    //         return this.hit = totalHits;
+            
+    //     })
+    // },
+//асинхронный код
+     async getFetch() {
+        
+        let queryParams = `?key=${this.apiKey}&q=${this.query}&image_type=photo&per_page=${this.per_page}&page=${this.page}&orientation=horizontal&safesearch=true`;
         //готовая строка запроса
         let url = this.baseUrl + queryParams;
         //собственно запрос, который будем обрабатывать методами then, потому что он возвращает промис
-        fetch(url).then((response) => {
-            //console.log(response);
-            return response.json();
-        }).then((hits) => {
-            console.log(hits);
-            let image = hits.hits;
-            let hit = hits.totalHits;
-            console.log(image);
-            console.log(hit);
-            return { image, hit };
-        }).then(({image, hit}) => {
-            console.log(image);
-            console.log(hit);
-            generateGallery(image, hit);
-        })
+        
+         const response = await fetch(url);
+         const hits = await response.json();
+         const image = hits.hits;
+         const totalHits = hits.totalHits;
+         generateGallery(image, totalHits);
+         return this.hit = totalHits;
+
+        //  fetch(url).then((response) => {
+        //     //console.log(response);
+        //     return response.json();
+        // }).then((hits) => {
+        //     console.log(hits);
+        //     let image = hits.hits;
+        //     let totalHits = hits.totalHits;
+        //     console.log(image);
+        //     console.log(totalHits);
+        //     return { image, totalHits };
+        // }).then(({ image, totalHits }) => {
+        //     console.log(image);
+        //     console.log(totalHits);
+        //     generateGallery(image, totalHits);
+        //     this.hit = totalHits;
+        //     console.log("кол-во картинок", totalHits)
+        //     console.log("хит",this.hit);
+        //     return this.hit = totalHits;
+            
+        // })
+    },
+    message() {
+        if (this.hit === 0) {
+            return;
+        }
+        console.log("message");
+        return Notiflix.Notify.info(`Hooray! We found ${this.hit} images.`);
     },
     loadMore(button) {
         button.addEventListener("click", () => {
@@ -106,3 +172,8 @@ const fetchObject = {
 };
 
 fetchObject.loadMore(more);
+
+
+//var images = $('.image').simpleLightbox();
+
+// images.next(); // Next Image

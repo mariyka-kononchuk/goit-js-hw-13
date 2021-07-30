869078cb7d3e92f0e90d268281bb7825aeb8f63a
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-07-30 15:48:22 +0300

    исправила ошибку в кнопке load more

--- src/js/axios-api.js
+++ src/js/axios-api.js
@@ -13,7 +13,7 @@ axios.defaults.baseURL = baseUrl;
 const apiKey = "22651538-53630abe578d2561aeb41817a";
 
 const myFetch = getFetch();
-const { setQuery, getImages, loadMore, resetPage, resetTotal, message } = myFetch;
+const { setQuery, getImages, loadMore, resetPage, resetTotal, resetHit, message } = myFetch;
 
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
@@ -28,6 +28,8 @@ form.addEventListener("submit", (evt) => {
     resetPage();
     //обнуляем значение total
     resetTotal();
+    //обнуляемя значение hit - количество найденных картинок за 1 запрос
+    resetHit();
     //записываем полученное значение из инпута в свойство объекта с запросом
     setQuery(query);
     //делаем запрос по значению из инпута и отрисовываем первый ответ
@@ -35,7 +37,7 @@ form.addEventListener("submit", (evt) => {
     //выводим сообщение о том, сколько нашли изображений
     setTimeout(() => {
         message();
-    }, 300);
+    }, 1000);
     //открываем кнопку загрузки
     more.classList.remove("is-hidden");
     //зачищаем инпут
@@ -53,6 +55,9 @@ function getFetch() {
 
     function resetTotal() {
         return total = 0;
+    }
+    function resetHit() {
+        return hit = 0;
     }
     function setPage() {
         return page += 1;
@@ -81,13 +86,15 @@ function getFetch() {
         const gallery = cardTpl(photo);
         //общее к-во выведенных картинок
         total += photo.length;
+        console.log(totalHits);
+        console.log(total);
         //если ничего не нашли по запросу (получили пустой массив)
         if (photo.length === 0) {
             more.classList.add("is-hidden");
             return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         }
         //если к-во отображенных картинок стало равно общему к-ву найденных по запросу картинок
-            else if (totalHits === total) {
+            else if (total >= totalHits) {
                 list.insertAdjacentHTML("beforeend", gallery);
                 more.classList.add("is-hidden");
                 return setTimeout(() => {
@@ -107,7 +114,7 @@ function getFetch() {
     function message() {
             if (hit === 0) {
                 return;
-            }
+        }
             return Notiflix.Notify.info(`Hooray! We found ${hit} images.`);
     }
     function loadMore(button) {
@@ -131,7 +138,7 @@ function getFetch() {
                     behavior: 'smooth',
                 });
     }
-    return { setQuery, loadMore, resetPage, getImages, message, resetTotal};
+    return { setQuery, loadMore, resetPage, getImages, message, resetTotal, resetHit};
 }
 
 
